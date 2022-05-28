d42473e2dafe4a7dbdd2a487cad6a75c687389af
duchuyle108 <duchuyle.it@gmail.com>
2022-05-28 23:39:25 +0200

    update filter consumer

--- new-source/filterer.py
+++ new-source/filterer.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     #Pulsar setup
     client = pulsar.Client('pulsar://localhost:6650')
     consumer = client.subscribe('DE2-repo', subscription_name='filter', consumer_type=_pulsar.ConsumerType.Shared)
-    # lang_producer = client.create_producer('DE2-lang')
-    # commit_producer = client.create_producer('DE2-commit')
+    lang_producer = client.create_producer('DE2-lang')
+    commit_producer = client.create_producer('DE2-commit')
     file_producer = client.create_producer('DE2-file')
     recv_count = 0
     send_count = 0
@@ -25,15 +25,15 @@ if __name__ == '__main__':
 
             # #Extract language and send to language topic
             repo_language = repo['primaryLanguage']['name']
-            # lang_producer.send(repo_language.encode('utf-8'))
-            # send_count += 1
+            lang_producer.send(repo_language.encode('utf-8'))
+            send_count += 1
 
             #Extract name/commit count list and craft message for commit topic
-            # commit_msg = {}
-            # commit_msg['name'] = repo['nameWithOwner']
-            # commit_msg['commit_count'] = repo['refs']['nodes']
-            # commit_producer.send(str(commit_msg).encode('utf-8'))
-            # send_count += 1
+            commit_msg = {}
+            commit_msg['name'] = repo['nameWithOwner']
+            commit_msg['commit_count'] = repo['refs']['nodes']
+            commit_producer.send(str(commit_msg).encode('utf-8'))
+            send_count += 1
 
             # #Extract language/file list and craft message for file topic
             file_msg = {}
