dc7f7675b223199276decd6bca52fe0969c16dcc
rai-prashanna <prai931024@gmail.com>
2022-05-30 21:44:05 +0200

    useful pulsar command-line

---
+++ docker/pulsario/working-steps.sh
@@ -0,0 +1,10 @@
+
+
+/pulsar/bin/pulsar-admin sink create --sink-type mongo --sink-config-file /home/mongodb-sink.yml --inputs test-mongo
+
+/pulsar/bin/pulsar-client produce -m "{firstname: \"P\", lastname: \"RAI\"}" -s % -n 10 test-mongo
+
+
+/pulsar/bin/pulsar-admin sink available-sinks
+/pulsar/bin/pulsar-admin sink list
+/pulsar/bin/pulsar-admin sink status --name mongo-test-sink
