25d5dd0a19fa684ceba9c39401767482008369cf
rai-prashanna <prai931024@gmail.com>
2022-05-30 10:54:32 +0200

    refactor

---
+++ docker/consumers/q2-consumer.py
@@ -0,0 +1,80 @@
+import pulsar, _pulsar
+import operator
+import sys
+import json
+import socket
+
+
+#Find corresponding positon for a number in a descending sorted list
+def find_position(repo_commits:list, n_commits:int):
+    # Check if list is empty
+    if not repo_commits:
+        return 0
+    new_pos = 0
+    for value in repo_commits:
+        if value >= n_commits:
+            new_pos += 1
+        else:
+            return new_pos
+    return new_pos
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("Program requires 1 input args: number of top repositories to retrieve!")
+        sys.exit(1)
+    n_top_repos = int(args[0])
+    #Pulsar setup
+    client = pulsar.Client('pulsar://localhost:6650')
+    consumer = client.subscribe('DE2-commit', subscription_name='DE-Q1', consumer_type=_pulsar.ConsumerType.Shared)
+    agg_producer = client.create_producer('DE2-agg')
+    
+    #List of repos and their corresponding number of commits
+    repo_list = []
+    repo_commits = []
+    #Aggregation message
+    agg_msg = {}
+    agg_msg['type'] = 'Q2'
+
+    msg_count = 0
+    frequency = 100 #frequency of printing top list/send update
+
+    while True:
+        msg = consumer.receive()
+        msg_count += 1
+        try:
+            content = msg.data().decode('utf-8').replace("'", '"')
+            repo = json.loads(content)
+            repo_name = repo['name']
+            #Check if repo already exists in list
+            if(repo_name in repo_list):
+                break
+            n_commits = 0
+            #count total of commits from all branches
+            for branch in list(repo['commit_count']):
+                n_commits += branch['target']['history']['totalCount']
+            #Find position for new repo in the list
+            repo_pos = find_position(repo_commits, n_commits)
+            #If not in top highest commits, do nothing
+            if repo_pos <= n_top_repos:
+                repo_list.insert(repo_pos, repo_name)
+                repo_commits.insert(repo_pos, n_commits)
+                #if list longer than n_top_repos, remove the last element from list
+                if len(repo_list) > n_top_repos:
+                    repo_list.pop()
+                    repo_commits.pop()
+                #Periodically print out list of languages and project counts
+            if msg_count % frequency == 0:
+                print("Current list of top %d repository with most commits from %d message:" %(n_top_repos, msg_count))
+                print(dict(zip(repo_list, repo_commits)))
+                #Craft message to the aggregation server
+                agg_msg['worker'] = socket.gethostname() #for agg server tell apart different replicas of a consumer
+                agg_msg['result'] = dict(zip(repo_list, repo_commits))
+                #Send aggregation message
+                agg_producer.send(str(agg_msg).encode('utf-8'))
+            consumer.acknowledge(msg)
+        except:
+            consumer.negative_acknowledge(msg)
+
+    # Destroy pulsar client
+    client.close()
---
+++ docker/consumers/q3-consumer.py
@@ -0,0 +1,65 @@
+import pulsar, _pulsar
+import re
+import json
+import socket
+
+
+keywords = ['test', 'spec']
+regex_expression = '(\s|^|\W|\d)' + "|".join(map(re.escape, keywords)) + '(\s|$|\W|\d)'
+
+def has_unit_test(list = list):
+    for unit in list:
+        #search for pattern in its name
+        if re.search(regex_expression, unit['name'], re.IGNORECASE):
+            return 1
+        #If being directory -> search inside
+        if unit['type'] == "tree":
+            sub_dir = unit['object']['entries']
+            for sub_unit in sub_dir:
+                if re.search(regex_expression, sub_unit['name'], re.IGNORECASE):
+                    return 1
+    return 0
+
+if __name__ == '__main__':
+    #Pulsar setup
+    client = pulsar.Client('pulsar://localhost:6650')
+    consumer = client.subscribe('DE2-file', subscription_name='DE-Q3', consumer_type=_pulsar.ConsumerType.Shared)
+    agg_producer = client.create_producer('DE2-agg')
+    #language list
+    language = {}
+    #Aggregation message
+    agg_msg = {}
+    agg_msg['type'] = 'Q3'
+
+    msg_count = 0
+    frequency = 100 #frequency of printing top list/send update
+
+    while True:
+        msg = consumer.receive()
+        msg_count += 1
+        try:
+            content = msg.data().decode('utf-8').replace("'", '"')
+            repo = json.loads(content)
+            repo_language = repo['language']
+            file_list = repo['file_list']
+            if has_unit_test(file_list):
+                if repo_language in language.keys():
+                    language[repo_language] += 1
+                else:
+                    language[repo_language] = 1
+            #Periodically print out list of languages and project counts
+            if msg_count % frequency == 1:
+                print("Current list of language count for repositories with unit-test from %d message:" %msg_count)
+                print(language)
+                #Craft message to the aggregation server
+                agg_msg['worker'] = socket.gethostname() #for agg server tell apart different replicas of a consumer
+                agg_msg['result'] = language
+                #Send aggregation message
+                agg_producer.send(str(agg_msg).encode('utf-8'))
+                
+            consumer.acknowledge(msg)
+        except:
+            consumer.negative_acknowledge(msg)
+
+    # Destroy pulsar client
+    client.close()
