ec75d24d8db2346fce64fadd492d0544d1147453
Tim Wywiol <tim-leon.wywiol.5831@student.uu.se>
2022-06-01 19:15:20 +0200

    frontend test with pulsar

--- docker/consumers/agg-server.py
+++ docker/consumers/agg-server.py
@@ -1,6 +1,7 @@
 import pulsar, _pulsar
 import operator
 import json
+from datetime import datetime
 
 PULSAR_IP = '192.168.2.139'
 
@@ -27,8 +28,11 @@ def agg_2(part: dict):
 
 if __name__ == '__main__':
     #Pulsar setup
-    client = pulsar.Client('pulsar://172.20.0.2:6650')
+    client = pulsar.Client('pulsar://' + PULSAR_IP + ':6650')
     consumer = client.subscribe('DE2-agg', subscription_name='DE-agg', consumer_type=_pulsar.ConsumerType.Exclusive)
+    producer = client.create_producer('DE2-result')    
+    ##List of producers to the listening topic
+    producer_list = []
     #Q1 list:
     Q1_part = {}
     Q1_final = {}
@@ -41,61 +45,104 @@ if __name__ == '__main__':
     #Q4 list:
     Q4_part = {}
     Q4_final = {}
-    i=0;
-    while i < 100000:
+
+    update_msg = {}
+
+    start_time = None
+
+    continue_flag = True
+    while continue_flag:
         msg = consumer.receive()
+        now = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
         try:
+            producer_name = msg.properties()['producer']
             content = msg.data().decode('utf-8').replace("'", '"')
-            update = json.loads(content)
-            worker = update['worker']
-            i=i+1; 
-            #Question 1
-            if update['type'] == 'Q1':
-                #Update to part list
-                Q1_part['worker'] = update['result']
-                #aggregate results
-                Q1_final = agg_1(Q1_part)
-                #Do smthing with the current updated result
-                print("**********************************")
-                print("Current result for Q1:")
-                print(Q1_final)
-            #Question 2
-            elif update['type'] == 'Q2':
-                #Update to part list
-                Q2_part['worker'] = update['result']
-                #Aggregate results
-                Q2_final = agg_2(Q2_part)
-                print("**********************************")
-                print("Current result for Q2:")
-                print(Q2_final)
-
-            #Question 3
-            elif update['type'] == 'Q3':
-                #Update to part list
-                Q3_part['worker'] = update['result']
-                #aggregate results
-                Q3_final = agg_1(Q3_part)
-                #Do smthing with the current updated result
-                print("**********************************")
-                print("Current result for Q3:")
-                print(Q3_final)
-
-            #Question 4
-            elif update['type'] == 'Q4':
-                #Update to part list
-                Q4_part['worker'] = update['result']
-                #aggregate results
-                Q4_final = agg_1(Q4_part)
-                #Do smthing with the current updated result
-                print("**********************************")
-                print("Current result for Q4:")
-                print(Q4_final)
-
+            #Receive start signal from request-producer
+            if content == 'start':
+                if start_time == None:
+                    start_time = datetime.now()
+                    print("[%s] Program starts!!!" %start_time)
+            
+            #Receive finish signal from producer
+            elif content == 'finish':
+                if producer_name in producer_list:
+                    print("[%s] A producer finished its job: %s" %(now, producer_name))
+                    producer_list.remove(producer_name) #Remove finished producer
+                    #If no producer is working
+                    if not producer_list:
+                        continue_flag = False
             else:
-                pass
+                if producer_name not in producer_list:
+                    print("[%s] New producer: %s" %(now, producer_name))
+                    producer_list.append(producer_name)
+                
+                update_msg['type'] = update['type']
+                update_msg['timestamp'] = now
+                update = json.loads(content)
+                #Question 1
+                if update['type'] == 'Q1':
+                    #Update to part list
+                    Q1_part[producer_name] = update['result']
+                    #aggregate results
+                    Q1_final = agg_1(Q1_part)
+                    #Do smthing with the current updated result
+                    print("**********************************")
+                    print("[%s]Update >> Current result for Q1:" %now)
+                    print(Q1_final)
+                    update_msg['result'] = Q1_final
+                #Question 2
+                elif update['type'] == 'Q2':
+                    #Update to part list
+                    Q2_part[producer_name] = update['result']
+                    #Aggregate results
+                    Q2_final = agg_2(Q2_part)
+                    print("**********************************")
+                    print("[%s]Update >> Current result for Q2:" %now)
+                    print(Q2_final)
+                    update_msg['result'] = Q2_final
+                #Question 3
+                elif update['type'] == 'Q3':
+                    #Update to part list
+                    Q3_part[producer_name] = update['result']
+                    #aggregate results
+                    Q3_final = agg_1(Q3_part)
+                    #Do smthing with the current updated result
+                    print("**********************************")
+                    print("[%s]Update >> Current result for Q3:" %now)
+                    print(Q3_final)
+                    update_msg['result'] = Q3_final
+                #Question 4
+                elif update['type'] == 'Q4':
+                    #Update to part list
+                    Q4_part[producer_name] = update['result']
+                    #aggregate results
+                    Q4_final = agg_1(Q4_part)
+                    #Do smthing with the current updated result
+                    print("**********************************")
+                    print("[%s]Update >> Current result for Q4:" %now)
+                    print(Q4_final)
+                    update_msg['result'] = Q4_final
+                
+                #Send to final topic for pulsarIO
+                producer.send(str(update_msg).encode('utf-8'))
+                
             consumer.acknowledge(msg)
         except:
             consumer.negative_acknowledge(msg)
 
+    print("Final result acquired!")
+    print("--------------Q1--------------")
+    print(Q1_final)
+    print("--------------Q2--------------")
+    print(Q2_final)
+    print("--------------Q3--------------")
+    print(Q3_final)
+    print("--------------Q4--------------")
+    print(Q4_final)
+    print("------------------------------")
+    print("Total amount of processing time: ", (datetime.now() - start_time).seconds ," seconds")
+    
     # Destroy pulsar client
+    producer.close()
+    consumer.close()
     client.close()
--- docker/consumers/q3-consumer.py
+++ docker/consumers/q3-consumer.py
@@ -1,8 +1,10 @@
 import pulsar, _pulsar
 import re
 import json
-import socket
+from datetime import datetime
+import time
 
+PULSAR_IP = '192.168.2.139'
 
 keywords = ['test', 'spec']
 regex_expression = '(\s|^|\W|\d)' + "|".join(map(re.escape, keywords)) + '(\s|$|\W|\d)'
@@ -22,11 +24,14 @@ def has_unit_test(list = list):
 
 if __name__ == '__main__':
     #Pulsar setup
-    client = pulsar.Client('pulsar://pulsarbroker:6650')
+    client = pulsar.Client('pulsar://' + PULSAR_IP + ':6650')
     consumer = client.subscribe('DE2-file', subscription_name='DE-Q3', consumer_type=_pulsar.ConsumerType.Shared)
     agg_producer = client.create_producer('DE2-agg')
+    agg_producer_name = agg_producer.producer_name()
     #language list
     language = {}
+    #List of producers to the listening topic
+    producer_list = []
     #Aggregation message
     agg_msg = {}
     agg_msg['type'] = 'Q3'
@@ -34,32 +39,55 @@ if __name__ == '__main__':
     msg_count = 0
     frequency = 100 #frequency of printing top list/send update
 
-    while True:
+    continue_flag = True
+    while continue_flag:
         msg = consumer.receive()
+        now = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
         msg_count += 1
         try:
+            producer_name = msg.properties()['producer']
             content = msg.data().decode('utf-8').replace("'", '"')
-            repo = json.loads(content)
-            repo_language = repo['language']
-            file_list = repo['file_list']
-            if has_unit_test(file_list):
-                if repo_language in language.keys():
-                    language[repo_language] += 1
-                else:
-                    language[repo_language] = 1
-            #Periodically print out list of languages and project counts
-            if msg_count % frequency == 1:
-                print("Current list of language count for repositories with unit-test from %d message:" %msg_count)
-                print(language)
-                #Craft message to the aggregation server
-                agg_msg['worker'] = socket.gethostname() #for agg server tell apart different replicas of a consumer
-                agg_msg['result'] = language
-                #Send aggregation message
-                agg_producer.send(str(agg_msg).encode('utf-8'))
+            #Receive finish signal from producer
+            if content == 'finish':
+                if producer_name in producer_list:
+                    print("[%s] A producer finished its job: %s" %(now, producer_name))
+                    producer_list.remove(producer_name) #Remove finished producer
+                    #If no producer is working
+                    if not producer_list:
+                        agg_msg['result'] = language
+                        #Update last time
+                        agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
+                        continue_flag = False
+            else:
+                if producer_name not in producer_list:
+                    print("[%s] New producer: %s" %(now, producer_name))
+                    producer_list.append(producer_name)
+                
+                repo = json.loads(content)
+                repo_language = repo['language']
+                file_list = repo['file_list']
+                if has_unit_test(file_list):
+                    if repo_language in language.keys():
+                        language[repo_language] += 1
+                    else:
+                        language[repo_language] = 1
+                #Periodically print out list of languages and project counts
+                if msg_count % frequency == 1:
+                    print("[%s]Current list of language count for repositories with unit-test from %d messages:" %(now, msg_count))
+                    print(language, "\n")
+                    #Craft message to the aggregation server
+                    agg_msg['result'] = language
+                    #Send aggregation message
+                    agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
                 
             consumer.acknowledge(msg)
         except:
             consumer.negative_acknowledge(msg)
 
+    time.sleep(1)
+    #Send ending signal to aggregation server
+    agg_producer.send("finish".encode('utf-8'), properties={'producer': agg_producer_name})
+    print("Fisnished all available jobs! Quitting...")
     # Destroy pulsar client
+    agg_producer.close()
     client.close()
