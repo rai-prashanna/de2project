b691597d89142a8baf5bcecbb04ca5f9ac27167b
duchuyle108 <duchuyle.it@gmail.com>
2022-06-02 23:24:50 +0200

    Remove old files

--- source/q1-consumer.py
+++ source/q1-consumer.py
@@ -1,36 +1,69 @@
-import pulsar
-import operator
-import sys
+import pulsar, _pulsar
+from datetime import datetime
+import time
+
+PULSAR_IP = '192.168.2.139'
 
 if __name__ == '__main__':
-    #Validate program arguments
-    args = sys.argv[1:]
-    if len(args) != 1:
-        print("Program requires 1 input arg: number of top repos")
-        sys.exit(1)
-    n_repos = args[0]
     #Pulsar setup
-    client = pulsar.Client('pulsar://localhost:6650')
-    consumer = client.subscribe('DE2-Q1', subscription_name='DE-sub')
+    client = pulsar.Client('pulsar://' + PULSAR_IP + ':6650')
+    consumer = client.subscribe('DE2-lang', subscription_name='DE-Q1', consumer_type=_pulsar.ConsumerType.Shared)
+    agg_producer = client.create_producer('DE2-agg')
+    agg_producer_name = agg_producer.producer_name()
     #language list
     language = {}
-    while True:
+    ##List of producers to the listening topic
+    producer_list = []
+    #Aggregation message
+    agg_msg = {}
+    agg_msg['type'] = 'Q1'
+    msg_count = 0
+    frequency = 100 #frequency of printing top list/send update
+
+    continue_flag = True
+    while continue_flag:
         msg = consumer.receive()
+        msg_count += 1
+        now = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
         try:
+            producer_name = msg.properties()['producer']
             content = msg.data().decode('utf-8')
-            if content == 'end-here': #receive end signal
-                #Sort language list in descending order of appearing times
-                sorted_list = sorted(language.items(),key=operator.itemgetter(1),reverse=True)
-                print("Analysis result: Top %s most used programming languages are" %n_repos)
-                print(sorted_list[0:int(n_repos)])
-                language = {}
-            elif content in language.keys():
-                language[content] += 1
+            #Receive finish signal from producer
+            if content == 'finish':
+                if producer_name in producer_list:
+                    print("[%s] A producer finished its job: %s" %(now, producer_name))
+                    producer_list.remove(producer_name) #Remove finished producer
+                    #If no producer is working
+                    if not producer_list:
+                        agg_msg['result'] = language
+                        #Update last time
+                        agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
+                        continue_flag = False
             else:
-                language[content] = 1
+                if producer_name not in producer_list:
+                    print("[%s] New producer: %s" %(now, producer_name))
+                    producer_list.append(producer_name)
+                
+                if content in language.keys():
+                    language[content] += 1
+                else:
+                    language[content] = 1
+                #Periodically print out list of languages and project counts
+                if msg_count % frequency == 1:
+                    print("[%s] Current list of language count from %d messages:" %(now,msg_count))
+                    print(language, "\n")
+                    #Craft message to the aggregation server
+                    agg_msg['result'] = language
+                    #Send aggregation message
+                    agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
             consumer.acknowledge(msg)
         except:
             consumer.negative_acknowledge(msg)
 
+    time.sleep(1)
+    #Send ending signal to aggregation server
+    agg_producer.send("finish".encode('utf-8'), properties={'producer': agg_producer_name})
+    print("Fisnished all available jobs! Quitting...")
     # Destroy pulsar client
+    agg_producer.close()
     client.close()
--- source/q2-consumer.py
+++ source/q2-consumer.py
@@ -1,15 +1,19 @@
-import pulsar
-import operator
+import pulsar, _pulsar
 import sys
+import json
+import time
+from datetime import datetime
 
+PULSAR_IP = '192.168.2.139'
 
+#Find corresponding positon for a number in a descending sorted list
 def find_position(repo_commits:list, n_commits:int):
     # Check if list is empty
     if not repo_commits:
         return 0
     new_pos = 0
     for value in repo_commits:
-        if value > n_commits:
+        if value >= n_commits:
             new_pos += 1
         else:
             return new_pos
@@ -18,47 +22,83 @@ def find_position(repo_commits:list, n_commits:int):
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) != 1:
-        print("Program requires 1 input args: number of repositories to retrieve!")
+        print("Program requires 1 input args: number of top repositories to retrieve!")
         sys.exit(1)
-    n_repos = int(args[0])
+    n_top_repos = int(args[0])
     #Pulsar setup
-    client = pulsar.Client('pulsar://localhost:6650')
-    consumer = client.subscribe('DE2-Q2', subscription_name='DE-Q2-sub')
+    client = pulsar.Client('pulsar://' + PULSAR_IP + ':6650')
+    consumer = client.subscribe('DE2-commit', subscription_name='DE-Q1', consumer_type=_pulsar.ConsumerType.Shared)
+    agg_producer = client.create_producer('DE2-agg')
+    agg_producer_name = agg_producer.producer_name()
     
     #List of repos and their corresponding number of commits
     repo_list = []
     repo_commits = []
+    #List of producers to the listening topic
+    producer_list = []
+    #Aggregation message
+    agg_msg = {}
+    agg_msg['type'] = 'Q2'
 
-    while True:
+    msg_count = 0
+    frequency = 100 #frequency of printing top list/send update
+
+    continue_flag = True
+    while continue_flag:
         msg = consumer.receive()
+        now = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
+        msg_count += 1
         try:
-            content = msg.data().decode('utf-8').split(';;;')
-            if content[0] == 'end-here': #receive end signal
-                #Sort language list in descending order of appearing times
-                print("Analysis result: Top %d most commited repositories are" %n_repos)
-                print(dict(zip(repo_list, repo_commits)))
-                repo_list = []
-                repo_commits = []
+            producer_name = msg.properties()['producer']
+            content = msg.data().decode('utf-8').replace("'", '"')
+            #Receive finish signal from producer
+            if content == 'finish':
+                if producer_name in producer_list:
+                    print("[%s] A producer finished its job: %s" %(now, producer_name))
+                    producer_list.remove(producer_name) #Remove finished producer
+                    #If no producer is working
+                    if not producer_list:
+                        #Update last time
+                        agg_msg['result'] = dict(zip(repo_list, repo_commits))
+                        agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
+                        continue_flag = False
             else:
-                repo_name = content[0]
-                n_commits = int(content[1])
+                if producer_name not in producer_list:
+                    print("[%s] New producer: %s" %(now, producer_name))
+                    producer_list.append(producer_name)
+                
+                repo = json.loads(content)
+                repo_name = repo['name']
+                #Check if repo already exists in list
+                if(repo_name in repo_list):
+                    continue
+                n_commits = repo['commit_count']
                 #Find position for new repo in the list
                 repo_pos = find_position(repo_commits, n_commits)
                 #If not in top highest commits, do nothing
-                if repo_pos >= n_repos:
-                    pass
-                #Otherwise, add to list
-                else:
+                if repo_pos <= n_top_repos:
                     repo_list.insert(repo_pos, repo_name)
                     repo_commits.insert(repo_pos, n_commits)
-                    #if list longer than n_repos, remove the last element from list
-                    if len(repo_list) > n_repos:
+                    #if list longer than n_top_repos, remove the last element from list
+                    if len(repo_list) > n_top_repos:
                         repo_list.pop()
                         repo_commits.pop()
-
+                    #Periodically print out list of languages and project counts
+                if msg_count % frequency == 0:
+                    print("[%s] Current list of top %d repositories with most commits from %d messages:" %(now, n_top_repos, msg_count))
+                    print(dict(zip(repo_list, repo_commits)), "\n")
+                    #Craft message to the aggregation server
+                    agg_msg['result'] = dict(zip(repo_list, repo_commits))
+                    #Send aggregation message
+                    agg_producer.send(str(agg_msg).encode('utf-8'), properties={'producer': agg_producer_name})
             consumer.acknowledge(msg)
         except:
             consumer.negative_acknowledge(msg)
 
+    time.sleep(1)
+    #Send ending signal to aggregation server
+    agg_producer.send("finish".encode('utf-8'), properties={'producer': agg_producer_name})
+    print("Fisnished all available jobs! Quitting...")
     # Destroy pulsar client
+    agg_producer.close()
     client.close()
