e097716a0eac7d9911765254d290516ee62bc202
rai-prashanna <prai931024@gmail.com>
2022-06-04 11:23:42 +0200

    init

--- docker/mongo/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-{
-      sleep 60
-        mongo lms --eval 'db.createUser({user:"admin", pwd:"admin", roles:[]});'
-} || {
-        echo "cmp already exist"
-}
--- standalone_with_docker/Consumers/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sleep 80  
-python /app/q1-consumer.py 10
